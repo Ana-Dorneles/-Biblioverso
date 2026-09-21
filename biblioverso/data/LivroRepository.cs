0c9c13072c462b0fdddb9668e71768762c279196
Ana-Dorneles <ana.dorneles4@aluno.educa.go.gov.br>
2026-09-21 18:01:43 +0000

    implemantação o objet e  LIVRO metodo andicionar

---
+++ biblioverso/data/LivroRepository.cs
@@ -0,0 +1,14 @@
+namespace biblioverso.Data
+{
+    public class LIvroRepository
+    {
+        private static List<Livro> _tabelaLivros = new List<Livro>();
+        private static int proximoId = 1; // variavel auxiliar 
+
+        public void Adicionar (Livro livro ) //método adicina livros na lista 
+    }
+       livro.Id = proximoId;++; 
+       _tabelaLivros.Add(livro);
+    
+}
+
---
+++ biblioverso/models/Livro.cs
@@ -0,0 +1,13 @@
+namespace  biblioverso.biblioverso 
+{
+    public class Livro 
+    {
+        public int Id {get; set;}
+        public string Titulo {get; set;}
+        public string Autor {get; set;}
+        public bool Emprestado {get; set;}
+    }
+
+}
+
+    
